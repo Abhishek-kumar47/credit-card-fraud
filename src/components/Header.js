910dd7ea8c25e988430258bd706bedf29075e869
Abhishek-kumar47 <kumar47abhishek1802@gmail.com>
2024-10-20 02:00:44 +0530

    authentication and signout button

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { signOut } from 'firebase/auth';
+import React from 'react'
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Header = () => {
+    const navigate = useNavigate(); 
+    const user = useSelector((store) => store.user);
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            navigate("/");
+            // Sign-out successful.
+          }).catch((error) => {
+            navigate("/error");
+            // An error happened.
+          });
+    }
+  return (
+  
+ 
+     <div className="w-screen px-8 py-2 flex justify-between">
+        <img src={user?.photoURL} alt="profile"/>
+      <button onClick={handleSignOut} className="bg-black text-white rounded-md w-28">Sign out</button>
+    </div>
+   
+  )
+};
+
+export default Header
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import React, { useRef, useState } from 'react'
-import LoginImage from '../assets/Login.png'
+import LoginImage from "../assets/Login.png"
 import { checkValidate } from '../utils/validate';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth"
 import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Login = () => {
     const [isLogInForm, setIsLogInForm]= useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    const name = useRef(null)
     const email = useRef(null);
     const password = useRef(null);
 
@@ -22,7 +28,21 @@ const Login = () => {
   .then((userCredential) => {
     // Signed up 
     const user = userCredential.user;
+    updateProfile(user, {
+        displayName: name.current.value, photoURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+      }).then(() => {
+        const {uid, email, displayName, photoURL} = auth.currentUser;
+        dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
+        navigate("/browse");
+        // Profile updated!
+        // ...
+      }).catch((error) => {
+        setErrorMessage(error.message);
+        // An error occurred
+        // ...
+      });
     console.log(user);
+
     // ...
   })
   .catch((error) => {
@@ -39,6 +59,7 @@ const Login = () => {
     // Signed in 
     const user = userCredential.user;
     console.log(user);
+    navigate("/browse");
     // ...
   })
   .catch((error) => {
@@ -62,7 +83,7 @@ const toggleLogInForm = () => {
         <form onSubmit={(e) => e.preventDefault()} className="text-white absolute w-3/12 my-36 mx-36 right-20 ">
             <h1 className="font-bold text-3xl py-2 my-2">{isLogInForm? "Login" : "Sign UP"}</h1>
             <h2 className="pb-2 mb-3">{isLogInForm? "Enter your email and password below" : "Enter Name, Email and Password below"}</h2>
-           {!isLogInForm && (<input type ="text" placeholder="Full Name" className="text-black p-4 my-4 w-full rounded-md"/>)}
+           {!isLogInForm && (<input ref={name} type ="text" placeholder="Full Name" className="text-black p-4 my-4 w-full rounded-md"/>)}
             <input ref={email} type ="text" placeholder="Email Address" className="text-black p-4 my-4 w-full rounded-md"/>
             <input ref={password} type="password" placeholder="Password" className="text-black p-4 my-4 w-full rounded-md"/>
             <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
